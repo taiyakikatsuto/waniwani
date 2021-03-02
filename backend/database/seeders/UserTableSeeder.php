9c249d0106aea720d81c963b4d1b98de8e8667a6
taiyakikatsuto <super.katsuto@gmail.com>
2021-03-02 14:24:37 +0900

    UserSeedの追加

---
+++ backend/database/seeders/UserTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'name' => 'test_user',
+                'email' => 'example@com',
+                'password' => Hash::make('password'),
+                'created_at' => date("Y-m-d H:i:s"),
+            ],
+        ]);
+        
+    }
+}
